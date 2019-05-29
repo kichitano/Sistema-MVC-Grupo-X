83c82e2fa921f301c2f35d9585c701a09ee2a140
kichitano <30935429+kichitano@users.noreply.github.com>
2019-05-29 16:39:08 -0500

    Insertar DetalleAsignacion
    Agregado el metodo de agregar registros en DetalleAsignacion

---
+++ Sistema_MVC_Grupo_X/Sistema_MVC_Grupo_X/Controllers/DetalleAsignacionController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sistema_MVC_Grupo_X.Models;
+
+namespace Sistema_MVC_Grupo_X.Controllers
+{
+    public class DetalleAsignacionController : Controller
+    {
+        private DetalleAsignacion objDetalleAsignacion = new DetalleAsignacion();
+        private Asignacion objAsignacion = new Asignacion();
+        private Docente objDocente = new Docente();
+        private Criterio objCriterio = new Criterio();
+        //GET
+        public ActionResult Index()
+        {
+            return View(objDetalleAsignacion.Listar());
+        }
+
+        //Action Visualizar
+        public ActionResult Visualizar(int id)
+        {
+            return View(objDetalleAsignacion.Obtener(id));
+        }
+
+        //Accion agregarEditar
+        public ActionResult AgregarEditar(int id = 0)
+        {
+            ViewBag.Asignacion = objAsignacion.Listar();
+            ViewBag.Docente = objDocente.Listar();
+            ViewBag.Criterio = objCriterio.Listar();
+            return View(
+                id == 0 ? new DetalleAsignacion() //Agrega un nuevo objeto
+                : objDetalleAsignacion.Obtener(id) //Devuelva un objeto
+                );
+        }
+
+        //Accion guardar
+        public ActionResult Guardar(int asignacion_id, int docente_id, int criterio_id, string estado)
+        {
+            objDetalleAsignacion.asignacion_id = asignacion_id;
+            objDetalleAsignacion.docente_id = docente_id;
+            objDetalleAsignacion.criterio_id = criterio_id;
+            objDetalleAsignacion.estado = estado;
+
+            if (ModelState.IsValid)
+            {
+                objDetalleAsignacion.Guardar();
+                return Redirect("~/Asignacion/AgregarEditar/"+asignacion_id);
+            }
+            else
+            {
+                return View("~/Views/Control/AgregarEditar.cshtml");
+            }
+        }
+
+        //Accion eliminar
+        public ActionResult Eliminar(int id)
+        {
+            objDetalleAsignacion.detalleasignacion_id = id;
+            objDetalleAsignacion.Eliminar();
+            return Redirect("~/Control");
+        }
+    }
+}
--- Sistema_MVC_Grupo_X/Sistema_MVC_Grupo_X/Models/DetalleAsignacion.cs
+++ Sistema_MVC_Grupo_X/Sistema_MVC_Grupo_X/Models/DetalleAsignacion.cs
@@ -28,5 +28,93 @@ namespace Sistema_MVC_Grupo_X.Models
         public virtual Criterio Criterio { get; set; }
 
         public virtual Docente Docente { get; set; }
+
+        //-----------------------------------------------------------------//
+
+        //metodo listar
+        public List<DetalleAsignacion> Listar() //Retorna un collection
+        {
+            var objDetalleAsignacion = new List<DetalleAsignacion>();
+            try
+            {
+                using (var db = new Modelo_Sistema())
+                {
+                    objDetalleAsignacion = db.DetalleAsignacion
+                        .Include("Asignacion")
+                        .Include("Docente")
+                        .Include("Criterio")
+                        .ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return objDetalleAsignacion;
+        }
+
+        //metodo obtener
+        public DetalleAsignacion Obtener(int id) //retorna solo un objeto
+        {
+            var objDetalleAsignacion = new DetalleAsignacion();
+            try
+            {
+                using (var db = new Modelo_Sistema())
+                {
+                    objDetalleAsignacion = db.DetalleAsignacion
+                        .Include("Asignacion")
+                        .Include("Docente")
+                        .Include("Criterio")
+                    .Where(x => x.detalleasignacion_id == id)
+                        .SingleOrDefault();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return objDetalleAsignacion;
+        }
+        //metodo guardar y modificar
+        public void Guardar()
+        {
+            try
+            {
+                using (var db = new Modelo_Sistema())
+                {
+                    if (this.detalleasignacion_id > 0)
+                    { //si existe un valor mayor a 0 es x que existe el registro
+                        db.Entry(this).State = EntityState.Modified;
+
+                    }
+                    else
+                    { //sino existe el registro lo graba (nuevo)
+                        db.Entry(this).State = EntityState.Added;
+                    }
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        //metodo eliminar
+        public void Eliminar()
+        {
+            try
+            {
+                using (var db = new Modelo_Sistema())
+                {
+                    db.Entry(this).State = EntityState.Deleted;
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
     }
 }
